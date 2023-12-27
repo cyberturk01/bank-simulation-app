42549fd706e9a900de4d40dcb1ee51e7d41e18f3
Gokhan Yigit <yigitgokhan1@gmail.com>
2023-12-27 14:00:19 +0100

    Security Config adjusted

--- src/main/java/org/yigit/BankSimulationAppApplication.java
+++ src/main/java/org/yigit/BankSimulationAppApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.yigit.dto.AccountDTO;
 import org.yigit.enums.AccountType;
 import org.yigit.service.AccountService;
@@ -40,6 +42,11 @@ public class BankSimulationAppApplication {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
